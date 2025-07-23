1e1f9b2b2582097b9a170d5d78d27c4817d55507
PraisesPJMT <praisesmusa@outlook.com>
2025-07-23 20:08:47 +0100

    feat: Add App.module.css and refactor App.tsx layout and styles

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,44 @@
+  import { useEffect, useState } from "react";
+import { ToastContainer } from 'react-toastify';
+  
+  import styles from "./App.module.css";
 
-import './App.module.css'
+import usePlans from "./hooks/usePlans";
+import PlanItem from "./components/plan-item/PlanItem";
+import Header from "./components/general/header/Header";
+import Filters from "./components/general/filters/Filters";
+import SideBar from "./components/general/side-bar/SideBar";
 
-function App() {
+import type { Plan } from "./types";
+
+const App: React.FC = () => {
+  const { filteredPlans } = usePlans();
+  const [items, setItems] = useState<Plan[]>([]);
+
+  useEffect(() => {
+    setItems(filteredPlans);
+  }, [filteredPlans]);
 
   return (
     <>
-      <h1>Plans</h1>
+      <div className={styles.app}>
+        <SideBar />
+        <div>
+          <Header />
+          <main>
+            <Filters />
+            <div className={styles.plans}>
+              {items.map((plan, index) => (
+                <PlanItem key={index} plan={plan} />
+              ))}
+            </div>
+          </main>
+        </div>
+      </div>
+      
+      <ToastContainer />
     </>
-  )
-}
+  );
+};
 
-export default App
+export default App;
